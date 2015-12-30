9d1a3c8255359ce15457d9c41f9b5a0b83e6e5fd
libenhe <libenhe919@163.com>
2015-12-30 08:58:53 +0800

    ContactdaoImpl测试类

---
+++ contactSystem_web/src/school/libenhe/contactSystem_web/test/TestContactDaoImpl.java
@@ -0,0 +1,76 @@
+package school.libenhe.contactSystem_web.test;
+
+import school.libenhe.contactSystem_web.dao.ContactDao;
+import school.libenhe.contactSystem_web.dao.impl.ContactDaoImpl;
+import school.libenhe.contactSystem_web.entity.Contact;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * ContactdaoImpl测试类
+ * 
+ * @author：LiBenhe E-mail：libenhe919@163.com
+ * @version： 2015-12-29 下午4:16:16
+ */
+public class TestContactDaoImpl {
+	
+ContactDao operator = null;
+	
+	/**
+	 * 初始化对象实例
+	 */
+	@Before
+	public void init(){
+		operator = new ContactDaoImpl();
+	}
+	
+
+	@Test
+	public void testAddContact(){
+		Contact contact = new Contact();
+		
+		contact.setName("张三2");
+		contact.setGender("男");
+		contact.setAge(20);
+		contact.setPhone("138888833333");
+		contact.setEmail("er888888ic@qq.com");
+		contact.setQq("38888884");
+		operator.addContact(contact);
+	}
+	
+	@Test
+	public void testUpdateContact(){
+		Contact contact = new Contact();
+		contact.setId("8579e0410bf7466a93f714365fce34e3"); //修改的ID
+		contact.setName("李四");
+		contact.setGender("女");
+		contact.setAge(30);
+		contact.setPhone("18888888888");
+		contact.setEmail("zhangsan@qq.com");
+		contact.setQq("338888884");
+		operator.updateContact(contact);
+	}
+	
+	@Test
+	public void testDeleteContact(){
+		operator.deleteContact("42dc576fbb4b4c6d849c87d532255d0c");
+	}
+	
+	@Test
+	public void testFindAll(){
+		List<Contact> list = operator.findAll();
+		for (Contact contact : list) {
+			System.out.println(contact);
+		}
+	}
+	
+	@Test
+	public void testFindById(){
+		Contact contact = operator.findById("8579e0410bf7466a93f714365fce34e3");
+		System.out.println(contact);
+	}
+}
