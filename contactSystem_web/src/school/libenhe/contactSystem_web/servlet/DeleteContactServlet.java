0dcee1bc95ed16d10bf8dc0ae997e1aa2365b080
libenhe <libenhe919@163.com>
2015-12-31 14:48:14 +0800

    删除联系人servlet

---
+++ contactSystem_web/src/school/libenhe/contactSystem_web/servlet/DeleteContactServlet.java
@@ -0,0 +1,47 @@
+package school.libenhe.contactSystem_web.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import school.libenhe.contactSystem_web.dao.ContactDao;
+import school.libenhe.contactSystem_web.dao.impl.ContactDaoImpl;
+
+
+/**
+* 删除联系人
+*
+* @project_name：contactSystem_web
+* @type_name：DeleteContactServlet
+* @author：LiBenhe   E-mail：libenhe919@163.com
+* @version： 2015-12-31 上午9:37:49
+*
+*/
+public class DeleteContactServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+			
+			//测试，火狐传递参数会传递两次，修改：在ContactDaoImpl删除方法中判断是否为null再进行删除，防止空指针
+			/*System.out.println("删除联系人执行");//调试
+			 * 
+			 * */
+			String id = request.getParameter("id");
+	        //删除联系人
+			ContactDao contactDao = new ContactDaoImpl();
+			contactDao.deleteContact(id);
+			//跳转，重定向会到查看所有联系人
+			response.sendRedirect(request.getContextPath()+"/ListContactServlet");
+	}
+
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+			doGet(request, response);
+	}
+
+}
