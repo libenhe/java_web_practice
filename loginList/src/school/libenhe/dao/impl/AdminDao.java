1307e06e562e2041c2f11e0b41d6568d17b879f1
libenhe <libenhe919@163.com>
2016-02-03 22:54:05 +0800

    loginList

---
+++ loginList/src/school/libenhe/dao/impl/AdminDao.java
@@ -0,0 +1,19 @@
+package school.libenhe.dao.impl;
+
+import school.libenhe.dao.IAdminDao;
+import school.libenhe.entity.Admin;
+
+/**
+ * @author：LiBenhe E-mail：libenhe919@163.com
+ * @version： 2016-2-3 下午10:14:27
+ */
+public class AdminDao implements IAdminDao{
+
+	@Override
+	public Admin findByName(Admin admin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
---
+++ loginList/src/school/libenhe/dao/impl/EmployeeDao.java
@@ -0,0 +1,22 @@
+package school.libenhe.dao.impl;
+
+import java.util.List;
+
+import school.libenhe.dao.IEmployeeDao;
+import school.libenhe.entity.Employee;
+
+/**
+ * @author：LiBenhe E-mail：libenhe919@163.com
+ * @version： 2016-2-3 下午10:17:47
+ */
+public class EmployeeDao implements IEmployeeDao{
+
+	@Override
+	public List<Employee> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
