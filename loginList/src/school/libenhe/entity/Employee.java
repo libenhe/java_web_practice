19614714af2316b4e71bfdfd8c212977c3b5f3eb
libenhe <libenhe919@163.com>
2016-02-03 22:52:11 +0800

    实验室学生实体类

---
+++ loginList/src/school/libenhe/entity/Employee.java
@@ -0,0 +1,39 @@
+package school.libenhe.entity;
+
+/**
+ * 实验室学生实体类
+ * 
+ * @author：LiBenhe E-mail：libenhe919@163.com
+ * @version： 2016-2-3 下午10:32:08
+ */
+public class Employee {
+
+	private int empId;
+	private String empName;
+	private int dept_id;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public int getDept_id() {
+		return dept_id;
+	}
+
+	public void setDept_id(int dept_id) {
+		this.dept_id = dept_id;
+	}
+
+}
