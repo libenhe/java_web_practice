263cea4479245cf35072ae62eaa75a58c9366769
libenhe <libenhe919@163.com>
2017-02-10 22:06:42 +0800

    实体类

---
+++ LoginInterceptor/src/school/libenhe/entity/Admin.java
@@ -0,0 +1,31 @@
+package school.libenhe.entity;
+
+/**
+ * @author Li Benhe Email: libenhe919@gmail.com
+ * @version 2017-2-10 下午12:33:52
+ */
+public class Admin {
+
+	private int id;
+	private String userName;
+	private String pwd;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	
+}
