695f99ca97b1088ec3e92347e1f3f4fa684f6a08
libenhe <libenhe919@163.com>
2017-02-11 22:18:52 +0800

    增加拦截器处理类

---
+++ LoginInterceptor/src/school/libenhe/interceptor/UserCheckInterceptor.java
@@ -0,0 +1,20 @@
+package school.libenhe.interceptor;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+/**
+ * @author Li Benhe Email: libenhe919@gmail.com
+ * @version 2017-2-11 下午10:11:44
+ */
+public class UserCheckInterceptor extends AbstractInterceptor{
+
+	/**
+	 * 拦截器处理
+	 */
+	public String intercept(ActionInvocation arg0) throws Exception {
+		
+		return null;
+	}
+
+}
