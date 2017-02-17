0bc490835d86c7b2e5d5e9aec81f0090f5fea06d
libenhe <libenhe919@163.com>
2017-02-17 20:35:47 +0800

    拦截器java文件

--- LoginInterceptor/src/school/libenhe/interceptor/UserCheckInterceptor.java
+++ LoginInterceptor/src/school/libenhe/interceptor/UserCheckInterceptor.java
@@ -1,6 +1,8 @@
 package school.libenhe.interceptor;
 
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.ActionProxy;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
 
 /**
@@ -12,9 +14,46 @@ public class UserCheckInterceptor extends AbstractInterceptor{
 	/**
 	 * 拦截器处理
 	 */
-	public String intercept(ActionInvocation arg0) throws Exception {
+	public String intercept(ActionInvocation invocation) throws Exception {
+		/**
+		 * 拿到当前执行的方法名，只要当前的方法名不是login，就进行验证
+		 */
 		
-		return null;
+		//获取ActionContext对象
+		ActionContext actionContext = invocation.getInvocationContext();
+		
+		//获取action的代理对象
+		ActionProxy actionProxy = invocation.getProxy();
+		
+		//获取当前执行的方法名
+		
+		String methodName = actionProxy.getMethod();
+		
+		//判断
+		if (!"login".equals(methodName)) {
+			//获取当前登录的用户
+				Object object = actionContext.getSession().get("userInfo");
+			if (object == null) {
+				//没有登陆
+				return"input";
+			} else {
+				// 已经登陆
+				return invocation.invoke();
+			} 
+		} else {
+			return invocation.invoke();
+		}
+		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
