27dcb2c91dde502812da98ea97b39191bb80ee6d
libenhe <libenhe919@163.com>
2017-04-18 20:59:31 +0800

    测试类

---
+++ hibernatecache/src/school/libenhe/onetoone/App.java
@@ -0,0 +1,12 @@
+package school.libenhe.onetoone;
+/**
+ * 
+ * @ClassName: App 
+ * @Description: 测试类
+ * @author Li Benhe Email: libenhe919@gmail.com
+ * @date 2017-4-18 下午3:37:53
+ *
+ */
+public class App {
+
+}
