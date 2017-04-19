30fb4b112c8da7e8d554b6dd2a8217c7a3587cfa
libenhe <libenhe919@163.com>
2017-04-19 20:50:04 +0800

    测试类

--- hibernatecache/src/school/libenhe/onetoone/App.java
+++ hibernatecache/src/school/libenhe/onetoone/App.java
@@ -1,4 +1,10 @@
 package school.libenhe.onetoone;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.junit.Test;
+
 /**
  * 
  * @ClassName: App 
@@ -9,4 +15,63 @@ package school.libenhe.onetoone;
  */
 public class App {
 
+	private static SessionFactory sf;
+	static {
+		sf = new Configuration()
+			.configure()
+			.addClass(User.class)   
+			.addClass(IdCard.class)   // 测试时候使用
+			.buildSessionFactory();
+	}
+	/**
+	 *  测试保存
+	 */
+	@Test
+	public void setSave() {
+		Session session = sf.openSession();
+		session.beginTransaction();
+		
+		User user = new User();
+		user.setUserName("李本贺");
+		
+		IdCard idCard = new IdCard();
+		idCard.setCardNum("230622XXXXXX7056");
+		idCard.setPlace("黑龙江");
+		
+		idCard.setUser(user);
+		
+		session.save(idCard);
+		
+		session.getTransaction().commit();
+		session.close();
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
