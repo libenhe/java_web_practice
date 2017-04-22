63b4a4ee8f2c32dd4df57ac3632011f7b4598c3e
libenhe <libenhe919@163.com>
2017-04-22 21:53:24 +0800

    实体类，子类

---
+++ hibernatecache/src/school/libenhe/extends1/Cat.java
@@ -0,0 +1,23 @@
+package school.libenhe.extends1;
+/**
+ * 
+ * @ClassName: Cat 
+ * @Description: 实体子类
+ * @author Li Benhe Email: libenhe919@gmail.com
+ * @date 2017-4-22 下午8:53:07
+ *
+ */
+public class Cat extends Animal {
+
+	private String eatMouse;
+
+	public String getEatMouse() {
+		return eatMouse;
+	}
+
+	public void setEatMouse(String eatMouse) {
+		this.eatMouse = eatMouse;
+	}
+	
+	
+}
