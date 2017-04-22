94766f5f57c7010f90265691659b5add1fa2e840
libenhe <libenhe919@163.com>
2017-04-22 21:54:02 +0800

    组合映射实体类

---
+++ hibernatecache/src/school/libenhe/component/Wheel.java
@@ -0,0 +1,27 @@
+package school.libenhe.component;
+/**
+ * 
+ * @ClassName: Wheel 
+ * @Description: 车轮类
+ * @author Li Benhe Email: libenhe919@gmail.com
+ * @date 2017-4-22 下午4:11:44
+ *
+ */
+public class Wheel {
+
+	private int count;
+	private int size;
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	
+}
