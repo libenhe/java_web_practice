f773e60afa45dee954c71823135a8c8754986618
libenhe <libenhe919@163.com>
2017-04-22 21:53:49 +0800

    继承映射抽象父类

---
+++ hibernatecache/src/school/libenhe/extends1/Animal.java
@@ -0,0 +1,32 @@
+package school.libenhe.extends1;
+
+/**
+ * 
+ * @ClassName: Animal
+ * @Description: 抽象类，父类
+ * @author Li Benhe Email: libenhe919@gmail.com
+ * @date 2017-4-22 下午9:05:13
+ * 
+ */
+public abstract class Animal {
+
+	private int id;
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
