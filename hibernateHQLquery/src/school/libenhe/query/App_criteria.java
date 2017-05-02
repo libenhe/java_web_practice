a19e98cee95bce356a8222912d420b89d41c7012
libenhe <libenhe919@163.com>
2017-05-02 21:04:50 +0800

    测试类

---
+++ hibernateHQLquery/src/school/libenhe/query/App_criteria.java
@@ -0,0 +1,13 @@
+package school.libenhe.query;
+/***
+ * 
+ * @ClassName: App_criteria 
+ * @Description: 测试类 Criteria查询
+ * @author Li Benhe Email: libenhe919@gmail.com
+ * @date 2017-5-2 下午8:27:05
+ *
+ */
+public class App_criteria {
+
+	
+}
