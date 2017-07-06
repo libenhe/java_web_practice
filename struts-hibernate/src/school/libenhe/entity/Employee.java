9cfdaa7d4fe93d922f378bebe96b3e31356af1c0
libenhe <libenhe919@163.com>
2017-07-06 20:52:02 +0800

    实体类

---
+++ struts-hibernate/src/school/libenhe/entity/Employee.java
@@ -0,0 +1,36 @@
+package school.libenhe.entity;
+
+public class Employee {
+
+	private int empId;
+	private String empName;
+	private double salary;
+	//多对一 员工与部门
+	private Dept dept;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public Dept getDept() {
+		return dept;
+	}
+	public void setDept(Dept dept) {
+		this.dept = dept;
+	}
+	
+	
+}
