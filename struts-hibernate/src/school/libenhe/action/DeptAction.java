dd03e2f2e886173f8f3a08e76f4cd5a112993c9a
libenhe <libenhe919@163.com>
2017-07-07 12:35:11 +0800

    action类

--- struts-hibernate/src/school/libenhe/action/DeptAction.java
+++ struts-hibernate/src/school/libenhe/action/DeptAction.java
@@ -1,5 +1,21 @@
 package school.libenhe.action;
 
-public class DeptAction {
+import school.libenhe.entity.Dept;
+import school.libenhe.service.DeptService;
 
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class DeptAction extends ActionSupport{
+
+	private DeptService deptService = new DeptService();
+	
+	public String execute() {
+		//主键查询
+		Dept dept = deptService.findById(5);//随便模拟个数据
+		//保存
+		ActionContext.getContext().getContextMap().put("dept", dept);
+		
+		return SUCCESS;
+	}
 }
