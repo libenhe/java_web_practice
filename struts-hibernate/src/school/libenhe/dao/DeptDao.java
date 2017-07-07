014f4ffce6734cd29bc2071043112731da3bf360
libenhe <libenhe919@163.com>
2017-07-07 12:34:58 +0800

    dao类

--- struts-hibernate/src/school/libenhe/dao/DeptDao.java
+++ struts-hibernate/src/school/libenhe/dao/DeptDao.java
@@ -1,5 +1,13 @@
 package school.libenhe.dao;
 
+import school.libenhe.entity.Dept;
+import school.libenhe.utils.HibernateUtils;
+
 public class DeptDao {
 
+	// 根据主键查询
+	public Dept findById(int id) {
+		//获取到session，执行操作
+		return (Dept) HibernateUtils.getSession().get(Dept.class, 1);
+	}
 }
