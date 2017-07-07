8465eb8fd833f7c338ef8dca4c4a82620e4821df
libenhe <libenhe919@163.com>
2017-07-07 12:34:24 +0800

    service类

--- struts-hibernate/src/school/libenhe/service/DeptService.java
+++ struts-hibernate/src/school/libenhe/service/DeptService.java
@@ -1,5 +1,14 @@
 package school.libenhe.service;
 
+import school.libenhe.dao.DeptDao;
+import school.libenhe.entity.Dept;
+
 public class DeptService {
 
+	//调用Dao
+	private DeptDao deptDao = new DeptDao();
+	
+	public Dept findById(int id) {
+		return deptDao.findById(id);
+	}
 }
