9f63dcfb93eb4286e3ffaf52f080caa91b9d9738
libenhe <libenhe919@163.com>
2017-07-23 22:01:04 +0800

    实体类

---
+++ ssh/src/school/libenhe/entity/Employee.java
@@ -0,0 +1,36 @@
+package school.libenhe.entity;
+
+public class Employee {
+
+	private int id;
+	private String empName;
+	private double salary;
+	private Dept dept;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public Dept getDept() {
+		return dept;
+	}
+	public void setDept(Dept dept) {
+		this.dept = dept;
+	}
+	
+}
