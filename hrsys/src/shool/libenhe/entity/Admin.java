07a4f47f19f986b028a0be49d754824d37167ee6
libenhe <libenhe919@163.com>
2017-07-25 20:41:56 +0800

    管理员实体类

---
+++ hrsys/src/shool/libenhe/entity/Admin.java
@@ -0,0 +1,28 @@
+package shool.libenhe.entity;
+
+public class Admin {
+
+	private int id;
+	private String adminName;
+	private String pwd;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAdminName() {
+		return adminName;
+	}
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	
+}
