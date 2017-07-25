8713e812a6f29e97d4c19df14d71e96d365cd131
libenhe <libenhe919@163.com>
2017-07-25 20:42:59 +0800

    员工模块接口设计

---
+++ hrsys/src/shool/libenhe/dao/IEmployee.java
@@ -0,0 +1,12 @@
+package shool.libenhe.dao;
+/**
+ * 
+ * @ClassName: IEmployee 
+ * @Description: 员工模块接口设计
+ * @author Li Benhe Email: libenhe919@gmail.com
+ * @date 2017-7-25 下午8:30:01
+ *
+ */
+public class IEmployee {
+
+}
