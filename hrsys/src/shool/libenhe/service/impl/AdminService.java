6c84020b7fad08170325eb5d9738afcc86b5054a
libenhe <libenhe919@163.com>
2017-08-01 21:02:36 +0800

    管理员逻辑接口实现类

--- hrsys/src/shool/libenhe/service/impl/AdminService.java
+++ hrsys/src/shool/libenhe/service/impl/AdminService.java
@@ -1,5 +1,26 @@
 package shool.libenhe.service.impl;
 
-public class AdminService {
+import shool.libenhe.dao.IAdminDao;
+import shool.libenhe.entity.Admin;
+import shool.libenhe.service.IAdminService;
+
+public class AdminService implements IAdminService {
+
+	private IAdminDao adminDao;
+
+	public void setAdminDao(IAdminDao adminDao) {
+		this.adminDao = adminDao;
+	}
+
+	@Override
+	public void register(Admin admin) {
+		adminDao.save(admin);
+
+	}
+
+	@Override
+	public Admin login(Admin admin) {
+		return adminDao.findByAdmin(admin);
+	}
 
 }
