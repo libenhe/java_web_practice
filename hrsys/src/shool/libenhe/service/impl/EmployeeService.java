df6aeaa9d3761b9258d2997050fd730ca2193efc
libenhe <libenhe919@163.com>
2017-08-01 21:03:00 +0800

    员工逻辑接口实现类

--- hrsys/src/shool/libenhe/service/impl/EmployeeService.java
+++ hrsys/src/shool/libenhe/service/impl/EmployeeService.java
@@ -1,5 +1,60 @@
 package shool.libenhe.service.impl;
 
-public class EmployeeService {
+import java.util.List;
+
+import shool.libenhe.dao.IEmployeeDao;
+import shool.libenhe.entity.Employee;
+import shool.libenhe.service.IEmployeeService;
+
+public class EmployeeService implements IEmployeeService {
+
+	private IEmployeeDao employeeDao;
+
+	public void setEmployeeDao(IEmployeeDao employeeDao) {
+		this.employeeDao = employeeDao;
+	}
+
+	@Override
+	public void delete(int id) {
+		employeeDao.delete(id);
+
+	}
+
+	@Override
+	public void save(Employee emp) {
+		employeeDao.save(emp);
+
+	}
+
+	@Override
+	public void update(Employee emp) {
+		employeeDao.update(emp);
+
+	}
+
+	@Override
+	public Employee findById(int id) {
+		return employeeDao.findById(id);
+	}
+
+	@Override
+	public List<Employee> getAll() {
+		return employeeDao.getAll();
+	}
+
+	@Override
+	public List<Employee> getAll(String employeeName) {
+		return employeeDao.getAll(employeeName);
+	}
+
+	@Override
+	public void deleteMany(int[] ids) {
+		if (ids != null && ids.length > 0) {
+			for (int id : ids) {
+				delete(id);
+			}
+		}
+
+	}
 
 }
