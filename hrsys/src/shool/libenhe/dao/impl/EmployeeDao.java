392b13797fa237aa94f72494a0b02786416ece84
libenhe <libenhe919@163.com>
2017-08-08 21:31:14 +0800

    更改部分继承BaseDao

--- hrsys/src/shool/libenhe/dao/impl/EmployeeDao.java
+++ hrsys/src/shool/libenhe/dao/impl/EmployeeDao.java
@@ -2,8 +2,6 @@ package shool.libenhe.dao.impl;
 
 import java.util.List;
 
-import org.hibernate.SessionFactory;
-
 import shool.libenhe.dao.IEmployeeDao;
 import shool.libenhe.entity.Employee;
 /**
@@ -14,57 +12,18 @@ import shool.libenhe.entity.Employee;
  * @date 2017-7-26 上午11:09:38
  *
  */
-public class EmployeeDao implements IEmployeeDao{
+public class EmployeeDao extends BaseDao<Employee> implements IEmployeeDao{
 
-	//容器注入
-    private SessionFactory sessionFactory;
-    
-    public void setSessionFactory(SessionFactory sessionFactory) {
-		this.sessionFactory = sessionFactory;
-	}
-    
-	@Override
-	public void save(Employee emp) {
-		sessionFactory.getCurrentSession().save(emp);
-		
-	}
+	
 
-	@Override
-	public void update(Employee emp) {
-		sessionFactory.getCurrentSession().update(emp);
-		
-	}
 
-	@Override
-	public void delete(int id) {
-		sessionFactory.getCurrentSession()//
-		              .createQuery("delete from Employee where id=?")//
-		              .setParameter(0, id)
-		              .executeUpdate();
-		
-	}
-
-	@Override
-	public Employee findById(int id) {
-		
-		return (Employee) sessionFactory.getCurrentSession().get(Employee.class, id);
-	}
-
+	
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<Employee> getAll(String employeeName) {
-		
-		return sessionFactory.getCurrentSession()//
-				.createQuery("from Employee where empName like ?")//
-				.setParameter(0, "%"+employeeName+"%")
-				.list();
+		return getSessionFactory().getCurrentSession()//
+			.createQuery("from Employee where empName like ?")//
+			.setParameter(0, "%" +employeeName + "%")//
+			.list();
 	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<Employee> getAll() {
-		
-		return sessionFactory.getCurrentSession().createQuery("from Employee").list();
-	}
-
 }
