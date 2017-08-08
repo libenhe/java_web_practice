a881a97798dc0709bccf1bf3f51eace35b10d101
libenhe <libenhe919@163.com>
2017-08-08 21:31:23 +0800

    管理员登陆注册模块

--- hrsys/src/shool/libenhe/action/AdminAction.java
+++ hrsys/src/shool/libenhe/action/AdminAction.java
@@ -1,5 +1,54 @@
 package shool.libenhe.action;
 
-public class AdminAction {
+import shool.libenhe.entity.Admin;
+import shool.libenhe.service.IAdminService;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+/**
+ * 
+ * @ClassName: AdminAction
+ * @Description: 管理员登陆注册模块
+ * @author Li Benhe Email: libenhe919@gmail.com
+ * @date 2017-8-8 下午4:46:23
+ * 
+ */
+public class AdminAction extends ActionSupport implements ModelDriven<Admin> {
+
+	// 封装请求数据
+	private Admin admin = new Admin();
+
+	public void setAdmin(Admin admin) {
+		this.admin = admin;
+	}
+
+	public Admin getAdmin() {
+		return admin;
+	}
+
+	@Override
+	public Admin getModel() {
+		return admin;
+	}
+
+	private IAdminService adminService;
+
+	public void setAdminService(IAdminService adminService) {
+		this.adminService = adminService;
+	}
+
+	public String login() {
+		Admin adminInfo = adminService.login(admin);
+		if (adminInfo == null) {
+			// 登陆失败
+			return "loginFaild";
+		} else {
+			// 登陆成功, 保存数据到session
+			ActionContext.getContext().getSession().put("adminInfo", adminInfo);
+			return "index";
+		}
+	}
 
 }
