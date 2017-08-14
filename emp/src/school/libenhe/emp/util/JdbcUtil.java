87bb4114d9ec740d521b36034d05f21fdf79d2b0
libenhe <libenhe919@163.com>
2017-08-14 20:25:00 +0800

    Jdbc工具类

--- emp/src/school/libenhe/emp/util/JdbcUtil.java
+++ emp/src/school/libenhe/emp/util/JdbcUtil.java
@@ -1,5 +1,21 @@
 package school.libenhe.emp.util;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+/**
+ * 
+ * @ClassName: JdbcUtil 
+ * @Description: Jdbc工具类
+ * @author Li Benhe Email: libenhe919@gmail.com
+ * @date 2017-8-14 上午10:44:54
+ *
+ */
 public class JdbcUtil {
 
+	//加载C3P0配置文件
+	private static ComboPooledDataSource  dataSource = new ComboPooledDataSource();
+	
+	public static ComboPooledDataSource getDataSource() {
+		return dataSource;
+	}
 }
