a4df4fd6567ba51bc6412d08954d671b82170731
libenhe <libenhe919@163.com>
2017-08-14 20:25:44 +0800

    逻辑层

--- emp/src/school/libenhe/emp/service/EmpSrevice.java
+++ emp/src/school/libenhe/emp/service/EmpSrevice.java
@@ -1,5 +1,19 @@
 package school.libenhe.emp.service;
 
+import school.libenhe.emp.dao.IEmpDao;
+import school.libenhe.emp.entity.Emp;
+
 public class EmpSrevice {
 
+	private IEmpDao empDao;
+	public void setEmpDao(IEmpDao empDao) {
+		this.empDao = empDao;
+	}
+	
+	/**
+	 * 增加员工
+	 */
+	public void register(Emp emp) throws Exception{
+		empDao.add(emp);
+	}
 }
