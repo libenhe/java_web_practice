73470620845e61e118bb592e44f1b121083cb54d
libenhe <libenhe919@163.com>
2017-08-25 20:35:58 +0800

    实体类

--- mybatistest/src/school/libenhe/mybatis/StudentDao.java
+++ mybatistest/src/school/libenhe/mybatis/StudentDao.java
@@ -1,5 +1,35 @@
 package school.libenhe.mybatis;
 
+import org.apache.ibatis.session.SqlSession;
+
 public class StudentDao {
 
+	public void add(Student student) throws Exception{
+		SqlSession sqlSession = null;
+		try{
+			sqlSession = MybatisUtil.getSqlSession();
+			//事务开始（默认）
+			//读取StudentMapper.xml映射文件中的SQL语句
+			sqlSession.insert(Student.class.getName()+".add",student);
+			//事务提交
+			sqlSession.commit();
+		}catch(Exception e){
+			e.printStackTrace();
+			//事务回滚
+			sqlSession.rollback();
+			throw e;
+		}finally{
+			MybatisUtil.closeSqlSession();
+		}
+	}
+	
+	
+	
+
+	public static void main(String[] args) throws Exception{
+		StudentDao dao = new StudentDao();
+		
+		dao.add(new Student(4,"libenhe",8000.3));
+		
+	}
 }
