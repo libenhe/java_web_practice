3bf8017478213373f91e935f1318a580ed462841
libenhe <libenhe919@163.com>
2017-09-17 21:31:45 +0800

    工具类

---
+++ mybatisdynamic/src/school/libenhe/mybatisdynamic/utils/MybatisUtil.java
@@ -0,0 +1,64 @@
+package school.libenhe.mybatisdynamic.utils;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.sql.Connection;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+
+public class MybatisUtil {
+	private static ThreadLocal<SqlSession> threadLocal = new ThreadLocal<SqlSession>();
+	private static SqlSessionFactory sqlSessionFactory;
+	
+	static{
+		try {
+			Reader reader = Resources.getResourceAsReader("mybatis.xml");
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+	
+	private MybatisUtil(){}
+	
+	public static SqlSession getSqlSession(){
+		
+		SqlSession sqlSession = threadLocal.get();
+		
+		if(sqlSession == null){
+			
+			sqlSession = sqlSessionFactory.openSession();
+			
+			threadLocal.set(sqlSession);
+		}
+		
+		return sqlSession;
+	}
+	
+	public static void closeSqlSession(){
+		
+		SqlSession sqlSession = threadLocal.get();
+	
+		if(sqlSession != null){
+			
+			sqlSession.close();
+			
+			threadLocal.remove();
+		}
+	}
+	
+	//测试是否连接
+	
+	public static void main(String[] args) {
+		Connection conn = MybatisUtil.getSqlSession().getConnection();
+		System.out.println(conn!=null?"连接成功":"连接失败");
+	}
+}
+
+
+
